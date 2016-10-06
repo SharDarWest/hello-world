8c747e91254c148a4634db7fa149ebb7fefdeac8
SharDarWest <shardarwest@gmail.com>
2016-10-06 14:10:58 -0400

    Added the poopy() function.

--- hello-world.cpp
+++ hello-world.cpp
@@ -10,12 +10,21 @@ void loopy(int times) {
     }
 }
 
+void poopy(int times) {
+
+    for (int i=1; i<times; ++i) {
+        cout << "Pooping, pooping, pooping." << endl;
+    }
+}
+
+
 
 int main (int argc, char *argv[])
 {
 
     cout << "Hello world!" << endl;
     loopy(11);
+    poopy(7);
     cout << "All Done!" << endl;
 
     return(0);
